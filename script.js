314cdd30744bbd750b9f99c548618cfdd6a01239
Dale Chou <dale@distract.org>
2023-03-08 17:30:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+const adjectives = [
+  'Mighty', 'Brave', 'Bold', 'Eternal', 'Radiant', 'Majestic', 'Divine', 'Noble', 'Fearless', 'Honorable'
+];
+
+const nouns = [
+  'Guardians', 'Dragons', 'Knights', 'Champions', 'Heroes', 'Paladins', 'Crusaders', 'Sentinels', 'Gladiators', 'Warriors'
+];
+
 const generateBtn = document.getElementById('generate-btn');
 generateBtn.addEventListener('click', function() {
-  fetch('https://wordsapiv1.p.rapidapi.com/words/?random=true&partOfSpeech=noun,adjective', {
-    headers: {
-      "x-rapidapi-key": "YOUR_API_KEY_HERE",
-      "x-rapidapi-host": "wordsapiv1.p.rapidapi.com"
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    const guildName = data.word + " " + data.results[0].synonyms[0];
-    const guildNameDiv = document.getElementById('guild-name');
-    guildNameDiv.textContent = guildName;
-  })
-  .catch(error => console.error(error));
+  const randomAdjectiveIndex = Math.floor(Math.random() * adjectives.length);
+  const randomNounIndex = Math.floor(Math.random() * nouns.length);
+  const guildName = adjectives[randomAdjectiveIndex] + " " + nouns[randomNounIndex];
+  const guildNameDiv = document.getElementById('guild-name');
+  guildNameDiv.textContent = guildName;
 });
